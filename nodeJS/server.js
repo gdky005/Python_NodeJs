2b84f53688726ee7e4b4250a1bfb18f83a3dcc15
qingw <qingw@jumei.com>
2017-02-11 08:23:31 +0800

    1. 启动服务器。

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,8 +1,13 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log("Request received.")
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for "+ pathname +" received.");
+
+        route(pathname);
+
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World,WangQing. 开心的说。")
         response.end();
@@ -14,3 +19,4 @@ function start() {
 
 
 exports.start = start;
+
