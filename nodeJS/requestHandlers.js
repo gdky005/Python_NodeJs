dbc34b944b12ad5805a06398b07fdd769cd83192
qingw <qingw@jumei.com>
2017-02-11 08:41:05 +0800

    1. 路由直接返回数据。

--- nodeJS/requestHandlers.js
+++ nodeJS/requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
     console.log("Request handler 'start' was called.");
+    return "hello Start";
 }
 
 function upload() {
     console.log("Request handler 'upload' was called.");
+    return "Hello Upload";
 }
 
 exports.start = start;
