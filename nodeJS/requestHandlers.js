516f85b3b99e97742ab20f38d300e334524c4319
qingw <qingw@jumei.com>
2017-02-13 01:15:58 +0800

    1. 在 Node.js 中 使用 Mock.js。

--- nodeJS/requestHandlers.js
+++ nodeJS/requestHandlers.js
@@ -1,5 +1,7 @@
 var exec = require("child_process").exec;
 
+var Mock = require('mockjs')
+
 function start(response) {
     console.log("Request handler 'start' was called.");
 
@@ -10,13 +12,30 @@ function start(response) {
     //     response.end();
     // })
 
-    exec("find /",
-        {timeout: 10000, maxBuffer: 20000 * 1024},
-        function (error, stdout, stderr) {
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write(stdout);
-            response.end();
-        });
+    // exec("find /",
+    //     {timeout: 10000, maxBuffer: 20000 * 1024},
+    //     function (error, stdout, stderr) {
+    //         response.writeHead(200, {"Content-Type": "text/plain"});
+    //         response.write(stdout);
+    //         response.end();
+    //     });
+
+
+    var data = Mock.mock({
+        // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+        'list|1-10': [{
+            // 属性 id 是一个自增数，起始值为 1，每次增 1
+            'id|+1': 1
+        }]
+    });
+
+    var responseData = JSON.stringify(data, null, 4);
+
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(responseData);
+    response.end();
+
 
 }
 
